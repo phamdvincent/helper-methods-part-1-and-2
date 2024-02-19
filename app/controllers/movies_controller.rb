6e8e8c0ff1a7961326f754dfee5774d269b07801
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-19 21:08:54 +0000

    Refactored new and create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
@@ -26,9 +26,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
+    # @movie.title = params.fetch(:title)
+    # @movie.description = params.fetch(:description)
 
     if @movie.valid?
       @movie.save
